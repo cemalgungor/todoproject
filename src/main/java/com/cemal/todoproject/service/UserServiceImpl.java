1ad12cb9befc03190ad3f0b3f2fa77f509f25a34
cemalgungor <cemalgngor@gmail.com>
2020-09-13 10:13:29 +0300

    new methods was added for service

--- src/main/java/com/cemal/todoproject/service/UserServiceImpl.java
+++ src/main/java/com/cemal/todoproject/service/UserServiceImpl.java
@@ -1,7 +1,27 @@
 package com.cemal.todoproject.service;
 
+import com.cemal.todoproject.entity.User;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements IUserService{
+    @Override
+    public User addUser() {
+        return null;
+    }
+
+    @Override
+    public User getUserById() {
+        return null;
+    }
+
+    @Override
+    public User updateUser() {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteUser() {
+        return null;
+    }
 }
