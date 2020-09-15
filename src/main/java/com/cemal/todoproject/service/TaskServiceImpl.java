86211fed168a85c40502f1e86f4f95df11459926
cemalgungor <cemalgngor@gmail.com>
2020-09-15 15:40:49 +0300

    TaskService implementation was developed

--- src/main/java/com/cemal/todoproject/service/TaskServiceImpl.java
+++ src/main/java/com/cemal/todoproject/service/TaskServiceImpl.java
@@ -1,31 +1,52 @@
 package com.cemal.todoproject.service;
 
 import com.cemal.todoproject.entity.Task;
+import com.cemal.todoproject.entity.TaskDetail;
+import com.cemal.todoproject.repository.ITaskRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TaskServiceImpl implements ITaskService{
+public class TaskServiceImpl implements ITaskService {
 
+    private final ITaskRepo taskRepo;
+
+
+    public TaskServiceImpl(ITaskRepo taskRepo) {
+        this.taskRepo = taskRepo;
+    }
 
     @Override
     public List<Task> getAllTask() {
-        return null;
+
+        return taskRepo.findAll();
     }
 
     @Override
-    public Task addTask() {
-        return null;
+    public Task addTask(Task task) {
+        TaskDetail td = new TaskDetail();
+        td.setId(task.getId());
+        task.setTaskDetail(td);
+        return taskRepo.save(task);
     }
 
     @Override
-    public Boolean deleteTask() {
-        return null;
+    public Boolean deleteTask(Long id) {
+        taskRepo.deleteById(id);
+        if(taskRepo.findById(id).equals(id)) {
+            return null;
+        }
+        return true;
     }
 
     @Override
-    public Task updateTask() {
-        return null;
+    public Task updateTask(Task task, Long id) {
+
+        Task taskupdate= taskRepo.getOne(id);
+        taskupdate.setTaskStatus(task.getTaskStatus());
+        taskupdate.setTitle(task.getTitle());
+        return taskRepo.save(taskupdate);
     }
 }
